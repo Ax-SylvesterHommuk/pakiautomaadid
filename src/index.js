8b66972c4acfd6eedfd11db6bbafca1e556266ed
Ax-SylvesterHommuk <ax-sylvester.hommuk@khk.ee>
2023-06-04 19:51:42 +0300

    Pakiautomaadid

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import cors from "cors";
+import express from "express";
+const app = express();
 
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
